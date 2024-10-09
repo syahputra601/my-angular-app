e08f7c88797103c49d0f95279b4bf402a17eace3
syahputra601 <ajisyahputra601@gmail.com>
2024-10-09 11:32:50 +0700

    added 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,7 +28,8 @@ export class AppComponent implements OnInit{
   //   {"user_name":"Syahputra","email":"syahputra@ymail.com"},
   //   {"user_name":"Firman","email":"firman@ymail.com"}
   // ];
-  //testing
+  showHeader_git: boolean = true;
+  //testing development
 
   searchControl = new FormControl(); //Control untuk input autocompolete
   filteredOptions: Observable<any[]>; // Observable untuk opsi yang terfilter
