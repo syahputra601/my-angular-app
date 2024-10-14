9a7d2508e4cd6ae736923e293ce8a96fa90cb10b
syahputra601 <ajisyahputra601@gmail.com>
2024-10-14 15:09:18 +0700

    menambahkan about routing dan about.module

---
+++ src/app/about/about-routing/about-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AboutRoutingRoutingModule } from './about-routing-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AboutRoutingRoutingModule
+  ]
+})
+export class AboutRoutingModule { }
