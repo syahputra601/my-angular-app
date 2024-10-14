a1c3e6eae09c161de178b024b2a382d670208a23
syahputra601 <ajisyahputra601@gmail.com>
2024-10-14 15:40:50 +0700

    edit 1

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MyComponentComponent } from './features/my-component.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MyComponentComponent],
   imports: [
     CommonModule,
   ]
